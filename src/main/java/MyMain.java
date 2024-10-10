da2d65378aadb60f117f87ae211d2a1264ea0e78
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-10-10 11:15:46 -0700

    started writing javadoc comments

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,19 +2,33 @@ import java.util.ArrayList;
 
 public class MyMain {
 
-    // Counts the number of words that starts with 'a' or 'A'
+    /**
+     * Counts the number of words that starts with 'a' or 'A'
+     * @param list A non-null, non-empty ArrayList of strings that are individual words containing only letters.
+     *             No words will be null or empty.
+     * @return The number of words starting with 'a' or 'A'. Min: 0, Max: <code>list.size()</code>.
+     */
     public static int startsWithA(ArrayList<String> list) {
         // REPLACE WITH YOUR CODE
         return 0;
     }
 
-    // Adds a ! to each element in the ArrayList, returns an ArrayList
+    /**
+     * Adds a ! to the end of each element in the ArrayList.
+     * @param list A non-null, non-empty ArrayList of strings. No words will be null or empty.
+     * @return The same list passed to the method.
+     */
     public static ArrayList<String> makeExciting(ArrayList<String> list) {
         // REPLACE WITH YOUR CODE
-        return null;
+        return list;
     }
 
-    // Returns true if there is an int that appears in both lists
+    /**
+     * Returns true if there is an int that appears in both lists
+     * @param list1
+     * @param list2
+     * @return
+     */
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) {
         // REPLACE WITH YOUR CODE
         return false;
