9cd024fef5017af54d005b2a56db9b228e19c6ca
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-10-14 11:20:04 -0700

    wrote all the javadoc comments

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -24,31 +24,45 @@ public class MyMain {
     }
 
     /**
-     * Returns true if there is an int that appears in both lists
-     * @param list1
-     * @param list2
-     * @return
+     * Returns true if there is an int that appears in both ArrayLists
+     * @param list1 An ArrayList of ints. Neither null nor empty.
+     * @param list2 An ArrayList of ints. Neither null nor empty.
+     * @return true if at least one int appears in both ArrayLists, false otherwise
      */
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) {
         // REPLACE WITH YOUR CODE
         return false;
     }
 
-    // Counts the number of odd numbers in list
+    /**
+     * Counts the number of odd numbers in list
+     * @param list An ArrayList of distinct ints. Neither null nor empty. May include negative numbers.
+     * @return How many odd integers there are in the list.
+     */
     public static int countOdd(ArrayList<Integer> list) {
         // REPLACE WITH YOUR CODE
         return 0;
     }
 
-    // Takes an int[] as input and returns the equivalent ArrayList<Integer>
-    // Unlike the other methods, the input is an array!
+    /**
+     * Takes an int[] as input and returns the equivalent ArrayList&lt;Integer>
+     * Unlike the other methods, the input is an array!
+     * @param arr An array of integers. length > 0.
+     * @return A new ArrayList&lt;Integer> that holds the same integers as <code>arr</code> in the same order
+     */
     public static ArrayList<Integer> convertToArrayList(int[] arr) {
         // REPLACE WITH YOUR CODE
         return null;
     }
 
-    // Takes two **sorted** ArrayLists and merges them together into one big sorted ArrayList
-    // (Hint: you may find it useful to use a while loop, as well as the remove() method).
+    /**
+     * Takes two <i>sorted</i> ArrayLists and merges them together into one big sorted ArrayList
+     * (Hint: you may find it useful to use a while loop, as well as the remove() method).
+     * You <i>can</i> change the contents of list1 and list2, but if you want a challenge, try to avoid it.
+     * @param list1 A sorted ArrayList of integers. Neither null nor empty.
+     * @param list2 A sorted ArrayList of integers. Neither null nor empty.
+     * @return A new ArrayList&lt;Integer> with all integers from list1 and list2 in ascending (sorted) order
+     */
     public static ArrayList<Integer> merge(ArrayList<Integer> list1, ArrayList<Integer> list2) {
         // REPLACE WITH YOUR CODE
         return null;
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTests {
 
@@ -52,8 +52,11 @@ public class MyTests {
         ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(new Integer[] {2, 4, 6, 8, 10}));
         ArrayList<Integer> list3 = new ArrayList<Integer>(Arrays.asList(new Integer[] {1, 4, 15, 21, 23, 26, 28}));
         ArrayList<Integer> list4 = new ArrayList<Integer>(Arrays.asList(new Integer[] {2, 5, 7, 8, 24, 37, 38}));
+        ArrayList<Integer> merged1 = MyMain.merge(list1, list2);
 
-        assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", MyMain.merge(list1, list2).toString(), "Merging the lists [1, 3, 5, 7, 9] and [2, 4, 6, 8, 10] should create: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+        assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", merged1.toString(), "Merging the lists [1, 3, 5, 7, 9] and [2, 4, 6, 8, 10] should create: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+        assertNotSame(merged1, list1);
+        assertNotSame(merged1, list2);
         assertEquals("[1, 2, 4, 5, 7, 8, 15, 21, 23, 24, 26, 28, 37, 38]", MyMain.merge(list3, list4).toString(), "Merging the lists [1, 4, 15, 21, 23, 26, 28] and [2, 5, 7, 8, 24, 37, 38] should create: [1, 2, 4, 5, 7, 8, 15, 21, 23, 24, 26, 28, 37, 28]");
     }
     
